f55c91e9b88f253a9061d8f6dc50368116a8c685
wangxuan <865387432@qq.com>
2015-08-20 21:13:20 +0800

    modify Parameter Signed-off-by: wangxuan <865387432@qq.com>

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 define("APP_PATH",  realpath(dirname(__FILE__) . '/../')); /* 指向public的上一级 */
 define("APPLICATION_PATH",  realpath(APP_PATH.'/application'));
 define("LIBRARY_PATH",  realpath(APP_PATH.'/application/library'));
-$app  = new Yaf_Application(APP_PATH . "/conf/application.ini",'smarty');
+$app  = new Yaf_Application(APP_PATH . "/conf/application.ini",'product');
 $app->bootstrap()->run();
 
